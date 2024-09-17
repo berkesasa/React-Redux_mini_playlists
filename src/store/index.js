11c7eab8eef359b7ffb662b0dd2157b7e5f46edc
berkesasa <berkesasa11@gmail.com>
2024-09-17 19:14:20 +0300

    state management and actions update

---
+++ src/store/index.js
@@ -0,0 +1,48 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const moviesSlice = createSlice({
+    name: 'movie',
+    initialState: [],
+    reducers: {
+        addMovie(state, action) {
+            state.push(action.payload)
+        },
+        removeMovie(state, action) {
+            const index = state.indexOf(action.payload)
+            state.splice(index, 1)
+        },
+        reset(state, action) {
+            return [];
+        }
+    }
+})
+
+const songSlice = createSlice({
+    name: 'song',
+    initialState: [],
+    reducers: {
+        addSong(state, action) {
+            state.push(action.payload)
+        },
+        removeSong(state, action) {
+            const index = state.indexOf(action.payload)
+            state.splice(index, 1)
+        }
+    },
+    extraReducers(builder) {
+        builder.addCase('movie/reset', (state, action) => {
+            return [];
+        })
+    }
+})
+
+const store = configureStore({
+    reducer: {
+        songs: songSlice.reducer,
+        movies: moviesSlice.reducer
+    }
+})
+
+export { store }
+export const { addSong, removeSong } = songSlice.actions;
+export const { addMovie, removeMovie, reset } = moviesSlice.actions;
