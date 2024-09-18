1747b526599bf47e69485fd73412a4ae222692b7
berkesasa <berkesasa11@gmail.com>
2024-09-18 19:53:37 +0300

    refactoring folder structure and added manual action

--- src/store/index.js
+++ src/store/index.js
@@ -1,48 +1,13 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-
-const moviesSlice = createSlice({
-    name: 'movie',
-    initialState: [],
-    reducers: {
-        addMovie(state, action) {
-            state.push(action.payload)
-        },
-        removeMovie(state, action) {
-            const index = state.indexOf(action.payload)
-            state.splice(index, 1)
-        },
-        reset(state, action) {
-            return [];
-        }
-    }
-})
-
-const songSlice = createSlice({
-    name: 'song',
-    initialState: [],
-    reducers: {
-        addSong(state, action) {
-            state.push(action.payload)
-        },
-        removeSong(state, action) {
-            const index = state.indexOf(action.payload)
-            state.splice(index, 1)
-        }
-    },
-    extraReducers(builder) {
-        builder.addCase('movie/reset', (state, action) => {
-            return [];
-        })
-    }
-})
+import { configureStore } from "@reduxjs/toolkit"
+import { songsReducer, addSong, removeSong } from "./slices/songsSlice"
+import { moviesReducer, addMovie, removeMovie } from "./slices/moviesSlice"
+import { reset } from "./actions"
 
 const store = configureStore({
     reducer: {
-        songs: songSlice.reducer,
-        movies: moviesSlice.reducer
+        songs: songsReducer,
+        movies: moviesReducer
     }
 })
 
-export { store }
-export const { addSong, removeSong } = songSlice.actions;
-export const { addMovie, removeMovie, reset } = moviesSlice.actions;
+export { store, reset, addSong, removeSong, addMovie, removeMovie }
